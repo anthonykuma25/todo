326f6f793eed2ff01a97499541552644774ac9e6
anthonykuma25 <anthonykuma2022@gmail.com>
2026-04-24 13:21:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,66 @@
-var todos = [];
+// get saved todos or empty array
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
-function add() {
-    var input = document.getElementById("input");
-    var value = input.value.trim();
+let input = document.getElementById("input");
+let list = document.getElementById("list");
 
-    if (value === "") {
-        alert("Enter something");
-        return;
-    }
+// show todos when page loads
+showTodos();
 
-    // prevent duplicate (case-insensitive)
-    var lowerValue = value.toLowerCase();
+function addTodo() {
+  let value = input.value;
 
-    for (var i = 0; i < todos.length; i++) {
-        if (todos[i].toLowerCase() === lowerValue) {
-            alert("This task already exists!");
-            return;
-        }
-    }
+  // prevent empty input
+  if (value === "") {
+    alert("Enter something");
+    return;
+  }
 
-    todos.push(value);
-    input.value = "";
-    show();
+  // prevent duplicates
+  if (todos.includes(value)) {
+    alert("Already exists");
+    return;
+  }
+
+  todos.push(value);
+  saveTodos();
+  showTodos();
+
+  input.value = "";
 }
 
-function show() {
-    var list = document.getElementById("list");
-    list.innerHTML = "";
+function showTodos() {
+  list.innerHTML = "";
 
-    for (var i = 0; i < todos.length; i++) {
-        list.innerHTML += `
-            <li>
-                ${todos[i]}
-                <button onclick="remove(${i})">x</button>
-            </li>
-        `;
-    }
-}
-function remove(i) {
-    todos.splice(i, 1);
-    show();
-}
-window.onload = function () {
-    var inputBox = document.getElementById("input");
+  for (let i = 0; i < todos.length; i++) {
 
-    inputBox.addEventListener("keydown", function (event) {
-        if (event.key === "Enter") {
-            add();
-        }
-    });
-};
+    let li = document.createElement("li");
+    li.innerText = todos[i];
+
+    // mark as done
+    li.onclick = function () {
+      li.style.textDecoration = "line-through";
+    };
+
+    // delete button
+    let btn = document.createElement("button");
+    btn.innerText = "X";
+
+    btn.onclick = function () {
+      removeTodo(i);
+    };
+
+    li.appendChild(btn);
+    list.appendChild(li);
+  }
+}
+
+function removeTodo(index) {
+  todos.splice(index, 1);
+  saveTodos();
+  showTodos();
+}
+
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
